ff24eb367e87df5438991f9cf9823b004e05ebed
David Shi <106148333@student.swin.edu.au>
2025-10-23 16:04:39 +1100

    minor adjustment to image_text

--- index.php
+++ index.php
@@ -16,16 +16,17 @@ include 'inc_files/error_alert.inc'; ?>
         <main>  <!--middle dragon image licensed from Adobe Stock images: 
                 https://as2.ftcdn.net/v2/jpg/07/23/38/53/1000_F_723385360_rEzKTrIuBEx9qu5PBJhgFH6wqlb3j4Pl.jpg-->
     <div class="dragon_image_container">
-        <img src="images/dragon_RPG.jpeg" alt="graphic_of_dragon_background"  class="dragon_image">            
-    </div>
+        <img src="images/dragon_RPG.jpeg" alt="graphic_of_dragon_background"  class="dragon_image">  
         <div class="image_text">
-                <h1>Timeless Classics, Forged in Darkness...</h1>
-                    <p id="company_description">
-                        With humble beginnings in Melbourne, Australia, SDAJ is a game development company that 
-                        creates new and innovative games with nostalgia at heart. From PC to mobile platforms, our games showcase our infinite imagination   
-                        that continues to delight players around the world.
-                    </p>
-        </div> 
+            <h1>Timeless Classics, Forged in Darkness...</h1>
+                <p>
+                    With humble beginnings in Melbourne, Australia, SDAJ is a game development company that 
+                    creates new and innovative games with nostalgia at heart. From PC to mobile platforms, our games showcase our infinite imagination   
+                    that continues to delight players around the world.
+                </p>
+        </div>  
+    </div>
+        
                 
 </main>
                     
