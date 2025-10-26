b5d34df4a12e7435418553bd1c590963d96243a7
STRIFE7301 <131334522+STRIFE7301@users.noreply.github.com>
2025-10-26 21:37:51 +1100

    spelling mistake for table eoi

--- process_eoi.php
+++ process_eoi.php
@@ -18,7 +18,7 @@
         postcode INT(4) NOT NULL,
         email VARCHAR(100) NOT NULL,
         phonenumber BIGINT(15) NOT NULL,
-        otherskill VARCHAR(100),
+        otherskills VARCHAR(100),
         value ENUM('New', 'Current', 'Final') DEFAULT 'New'
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
     ";
